1c15a1e96f6088898208eea2f37691957429e372
Lee-clipse <79911816+Lee-clipse@users.noreply.github.com>
2024-08-13 02:22:13 +0900

    feat: 연습문제 02

---
+++ react-lesson/src/lesson/연습문제/문제_02.jsx
@@ -0,0 +1,23 @@
+import React, { useState } from "react";
+
+function Index0002() {
+  // TODO
+
+  return (
+    <div>
+      <div id="lesson-title">문제 2: TODO LIST</div>
+      <div id="lesson-content">
+        <div className="title">문제</div>
+        <div>TODO LIST를 구현하시오.</div>
+        <div>할 일을 input 태그로 입력 받을 수 있다.</div>
+        <div>각 할 일은 작성된 현재 시간을 함께 저장하고 있다.</div>
+        <div>[추가] 버튼을 클릭하면 할 일을 목록에 추가할 수 있다.</div>
+        <div>[삭제] 버튼을 클릭하면 할 일을 목록에서 삭제할 수 있다.</div>
+        <div>검색 창에 할 일의 제목을 검색하면 검색 결과를 볼 수 있다.</div>
+        <div>목록은 화면에 렌더링된다.</div>
+      </div>
+    </div>
+  );
+}
+
+export default Index0002;
